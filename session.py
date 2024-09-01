9883f68310652e06c95c16db342bcf61b3f257a6
katikraavi <kati.kraavi@gmail.com>
2024-09-01 14:26:43 +0300

    added broken files

--- session.py
+++ session.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
+
 URI = "mysql+mysqlconnector://root:secret@localhost:3306/startup"
 # URI = "sqlite:///startup.db"
 engine = create_engine(URI)
